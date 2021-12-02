1ac8ccc02f128a9922fdeddac20ba5ff89ce4fc5
Tim Honchel <tim.honchel@gmail.com>
2021-12-02 15:52:29 -0500

    console app

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+            string name = "Tim";
+            int age = 31;
+            char initial = 'H';
+            bool truth = true;
+            double height = 71.5;
+            decimal weight = 190m;
+
+            Console.WriteLine("Hi, it is " + truth + " my name is " + name + " " + initial + " and I am " + age + " years old, " + height + " inches tall, and weigh " + weight + " pounds.");
